2674dbc46c44ee548474171d1cde6b4dcba5bae3
izzie <easy23n@gmail.com>
2019-05-01 20:28:07 +0900

    index test

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
  
 // https://pro-self-studier.tistory.com/19
 const title = 'hello WOrld!! git React Webpack Babel Setup!!!!!!!!!!!!!!!';
  
 ReactDOM.render(
-  <div>{title}</div>,
+  <App/>,
   document.getElementById('root')
 ); 
 
