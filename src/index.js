e3560f725dfa5d70d22068e2f923076c4f8edff6
MinHye-Jo <wjdska245@gmail.com>
2019-05-01 17:49:56 +0900

    테스트

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
  
 // https://pro-self-studier.tistory.com/19
-const title = 'wed23ad343434s React Webpack Babel Setup';
+const title = 'hello WOrld!! git React Webpack Babel Setup!!!!!!!!!!!!!!!';
  
 ReactDOM.render(
   <div>{title}</div>,
